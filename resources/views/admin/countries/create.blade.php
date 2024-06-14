5e1078ee3b53af18447a979764e95c48595f9c96
donarazhar <donarazhar@gmail.com>
2024-06-14 14:07:40 +0700

    Tambah data city dan country

--- resources/views/admin/countries/create.blade.php
+++ resources/views/admin/countries/create.blade.php
@@ -7,17 +7,18 @@
 
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">  
-                <form method="POST" action=" " enctype="multipart/form-data"> 
-
+            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
+                <form method="POST" action="{{ route('admin.countries.store') }}" enctype="multipart/form-data">
+                    @csrf
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                            :value="old('name')" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
-            
+
                         <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                             Add New Country
                         </button>
